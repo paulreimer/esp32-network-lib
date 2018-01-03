15bc5d7fc0d6555bac527993dbd623683a30a5d1
Paul Reimer <paul@p-rimes.net>
2018-01-02 21:16:24 -0800

    🔧 Set '#define HAVE_ZLIB_H 1' in curl_config.h to enable gzip support

--- curl_config.h
+++ curl_config.h
@@ -421,7 +421,7 @@
 //#define HAVE_LIBSSL 1
 
 /* if zlib is available */
-//#define HAVE_LIBZ 1
+#define HAVE_LIBZ 1
 
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
@@ -786,7 +786,7 @@
 /* #undef HAVE_X509_H */
 
 /* if you have the zlib.h header file */
-//#define HAVE_ZLIB_H 1
+#define HAVE_ZLIB_H 1
 
 /* Define to the sub-directory where libtool stores uninstalled libraries. */
 #define LT_OBJDIR ".libs/"
