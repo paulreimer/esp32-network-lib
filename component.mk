48ce922f6db313bfc48bc03bb82551842c51c63f
Paul Reimer <paul@p-rimes.net>
2018-01-09 10:27:26 -0800

    🔧 Add yajl, yajl/src and yajl/include to component.mk

--- component.mk
+++ component.mk
@@ -3,12 +3,14 @@
 #
 
 COMPONENT_ADD_INCLUDEDIRS := \
+	yajl/include \
 	curl/include
 
 COMPONENT_PRIV_INCLUDEDIRS := \
 	. \
 	curl/lib \
 	curl/include \
+	yajl \
 	zlib
 
 COMPONENT_SRCDIRS := \
@@ -16,6 +18,7 @@ COMPONENT_SRCDIRS := \
 	curl/lib \
 	curl/lib/vauth \
 	curl/lib/vtls \
+	yajl/src \
 	zlib
 
 CFLAGS += \
