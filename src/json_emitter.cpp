c6b7248d1225b58f59cdd5ccfd18117137a992ad
Paul Reimer <paul@p-rimes.net>
2018-01-19 19:40:23 -0800

    ✨ Add JsonEmitter class which uses yajl to parse streaming JSON and buffer the output at a specified path to a new JSON string

---
+++ src/json_emitter.cpp
@@ -0,0 +1,340 @@
+/*
+ * Copyright Paul Reimer, 2018
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License.
+ * To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+#include "json_emitter.h"
+
+#include <cstdio>
+
+yajl_callbacks json_parse_callbacks = {
+  // null
+  [] (void* ctx) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_null(); },
+
+  // boolean
+  [] (void* ctx, int b) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_boolean(b); },
+
+  // integer
+  nullptr,
+
+  // double
+  nullptr,
+
+  // number
+  [] (void* ctx, const char* s, size_t l) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_number(s, l); },
+
+  // string
+  [] (void* ctx, const unsigned char* s, size_t l) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_string(s, l); },
+
+  // start_map
+  [] (void* ctx) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_start_map(); },
+
+  // map_key
+  [] (void* ctx, const unsigned char* s, size_t l) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_map_key(s, l); },
+
+  // end_map
+  [] (void* ctx) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_end_map(); },
+
+  // start_array
+  [] (void* ctx) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_start_array(); },
+
+  // end_array
+  [] (void* ctx) -> int
+  { return static_cast<JsonEmitter*>(ctx)->on_json_parse_end_array(); },
+};
+
+JsonEmitter::JsonEmitter(const JsonPath& _match_path)
+: match_path(_match_path)
+, json_parser(yajl_alloc(&json_parse_callbacks, nullptr, this), yajl_free)
+, json_gen(yajl_gen_alloc(nullptr), yajl_gen_free)
+{
+  auto* g = json_gen.get();
+  yajl_gen_config(g, yajl_gen_beautify, 0);
+  yajl_gen_config(g, yajl_gen_validate_utf8, 1);
+
+  auto* hand = json_parser.get();
+  yajl_config(hand, yajl_allow_comments, 1);
+}
+
+bool
+JsonEmitter::parse(
+  std::experimental::string_view chunk,
+  Callback&& _callback,
+  Errback&& _errback
+)
+{
+  callback = _callback;
+  errback = _errback;
+
+  auto* hand = json_parser.get();
+  auto stat = yajl_parse(
+    hand,
+    reinterpret_cast<const unsigned char*>(chunk.data()),
+    chunk.size()
+  );
+
+  auto ok = (stat == yajl_status_ok);
+
+  return ok;
+}
+
+bool
+JsonEmitter::emit()
+{
+  auto* g = json_gen.get();
+
+  const unsigned char* buf;
+  size_t len;
+
+  yajl_gen_get_buf(g, &buf, &len);
+
+  auto json_str = std::experimental::string_view(
+    reinterpret_cast<const char*>(buf),
+    len
+  );
+
+  bool parsing_error = false;
+
+  if (not parsing_error)
+  {
+    callback(json_str);
+  }
+  else {
+    errback(json_str);
+  }
+
+  json_gen.reset(yajl_gen_alloc(nullptr));
+
+  return (not json_str.empty());
+}
+
+int
+JsonEmitter::on_json_parse_null()
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_null(g));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_boolean(int boolean)
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_bool(g, boolean));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_number(const char* s, size_t l)
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_number(g, s, l));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_string(const unsigned char* stringVal, size_t stringLen)
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_string(g, stringVal, stringLen));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_map_key(const unsigned char* stringVal, size_t stringLen)
+{
+  auto ok = true;
+
+  if (not current_path.empty())
+  {
+    current_path.pop_back();
+  }
+
+  current_path.push_back(
+    std::experimental::string_view(reinterpret_cast<const char*>(stringVal),
+    stringLen)
+  );
+
+  //if (is_a_subpath(current_path, match_path))
+  if (is_a_strict_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_string(g, stringVal, stringLen));
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_start_map()
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_map_open(g));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  current_path.push_back(std::string(""));
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_end_map()
+{
+  auto ok = true;
+
+  auto was_a_subpath = is_a_strict_subpath(current_path, match_path);
+  if (was_a_subpath)
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_map_close(g));
+  }
+
+  if (not current_path.empty())
+  {
+    current_path.pop_back();
+  }
+
+  auto still_a_subpath = is_a_strict_subpath(current_path, match_path);
+  if (was_a_subpath and not still_a_subpath)
+  {
+    emit();
+  }
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_start_array()
+{
+  auto ok = true;
+
+  if (is_a_subpath(current_path, match_path))
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_array_open(g));
+  }
+
+  if (
+    not current_path.empty() &&
+    stx::holds_alternative<int>(current_path.back())
+  )
+  {
+    auto& array_idx = stx::get<int>(current_path.back());
+    array_idx++;
+  }
+
+  current_path.push_back(0);
+
+  return ok;
+}
+
+int
+JsonEmitter::on_json_parse_end_array()
+{
+  auto ok = true;
+
+  auto was_a_subpath = is_a_strict_subpath(current_path, match_path);
+  if (was_a_subpath)
+  {
+    auto* g = json_gen.get();
+    ok = (yajl_gen_status_ok == yajl_gen_array_close(g));
+  }
+
+  if (not current_path.empty())
+  {
+    current_path.pop_back();
+  }
+
+  auto still_a_subpath = is_a_strict_subpath(current_path, match_path);
+  if (was_a_subpath and not still_a_subpath)
+  {
+    emit();
+  }
+
+  return ok;
+}
---
+++ src/json_emitter.h
@@ -0,0 +1,120 @@
+/*
+ * Copyright Paul Reimer, 2018
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License.
+ * To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
+#pragma once
+
+#include "response.h"
+
+#include <experimental/string_view>
+#include "stx/variant.hpp"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "yajl/yajl_parse.h"
+#include "yajl/yajl_gen.h"
+
+class JsonEmitter
+{
+public:
+  typedef stx::variant<int, std::string> JsonPathComponent;
+  typedef std::vector<JsonPathComponent> JsonPath;
+
+  typedef delegate<bool(std::experimental::string_view)> Callback;
+  typedef delegate<bool(std::experimental::string_view)> Errback;
+
+  JsonEmitter(const JsonPath& _match_path={});
+  ~JsonEmitter() = default;
+
+  bool parse(
+    std::experimental::string_view chunk,
+    Callback&& _callback,
+    Errback&& _errback
+  );
+
+protected:
+//TODO: make this protected
+public:
+  int on_json_parse_null();
+  int on_json_parse_boolean(int boolean);
+  int on_json_parse_number(const char* s, size_t l);
+  int on_json_parse_string(const unsigned char* stringVal, size_t stringLen);
+  int on_json_parse_map_key(const unsigned char* stringVal, size_t stringLen);
+  int on_json_parse_start_map();
+  int on_json_parse_end_map();
+  int on_json_parse_start_array();
+  int on_json_parse_end_array();
+
+private:
+  // Convenience function to output JSON, then call the stored callback/errback
+  bool emit();
+
+  // Emit JSON if path components are matched
+  JsonPath match_path;
+
+  // Input parsing state
+  JsonPath current_path;
+
+  // Callback/errback triggered after each parsing emit event completed
+  Callback callback;
+  Errback errback;
+
+  std::unique_ptr<yajl_handle_t, void(*)(yajl_handle_t*)> json_parser;
+  std::unique_ptr<yajl_gen_t, void(*)(yajl_gen_t*)> json_gen;
+
+  inline static bool
+  path_component_equality_or_wildcard(
+    JsonPathComponent root,
+    JsonPathComponent current
+  )
+  {
+    const JsonPathComponent wildcard_str("*");
+    const JsonPathComponent wildcard_int(-1);
+
+    // Accept only wildcards which used the correct wildcard type
+    return (
+      (
+       (root.index() == current.index()) &&
+       (root == wildcard_int || root == wildcard_str)
+      ) ||
+      (root == current)
+    );
+  }
+
+  inline static bool
+  is_a_subpath(
+    const JsonPath& current_path,
+    const JsonPath& match_path
+  )
+  {
+    return (
+      (match_path.empty()) || (
+      (current_path.size() >= match_path.size()) &&
+      (std::equal(
+        match_path.begin(),
+        match_path.end(),
+        current_path.begin(),
+        path_component_equality_or_wildcard
+      ))
+    ));
+  }
+
+  inline static bool
+  is_a_strict_subpath(
+    const JsonPath& current_path,
+    const JsonPath& match_path
+  )
+  {
+    return (
+      (is_a_subpath(current_path, match_path)) &&
+      (current_path.size() > match_path.size())
+    );
+  }
+};
