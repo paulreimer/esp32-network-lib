dfb5c1ae690ab6b704f934dd1037d50abfb344f2
Paul Reimer <paul@p-rimes.net>
2018-02-27 22:22:55 -0800

    ✨ Add actor_model.fbs flatbuffer schema file to actor_model component, and add a component.mk rule to generate actor_model_generated.h from it

--- actor_model/component.mk
+++ actor_model/component.mk
@@ -7,3 +7,13 @@ COMPONENT_ADD_INCLUDEDIRS := \
 
 COMPONENT_SRCDIRS := \
 	src
+
+$(COMPONENT_PATH)/src/actor_model_generated.h: $(COMPONENT_PATH)/actor_model.fbs
+	flatc --cpp -o $(@D) \
+	--scoped-enums \
+	--gen-mutable \
+	--gen-object-api \
+	--gen-name-strings \
+	--reflect-types \
+	--reflect-names \
+	$^
