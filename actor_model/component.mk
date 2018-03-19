35c66c6ba136248f27de3e45902b32284ba94f5c
Paul Reimer <paul@p-rimes.net>
2018-03-19 15:52:25 -0700

    🚚 Move generated flatbuffers output into actor_model/src/gen/

--- actor_model/component.mk
+++ actor_model/component.mk
@@ -4,15 +4,16 @@
 
 COMPONENT_ADD_INCLUDEDIRS := \
 	src \
+	src/gen \
 	lib/sole
 
 COMPONENT_SRCDIRS := \
 	src
 
-src/actor.o: $(COMPONENT_PATH)/src/actor_model_generated.h
-src/mailbox.o: $(COMPONENT_PATH)/src/actor_model_generated.h
+src/actor.o: $(COMPONENT_PATH)/src/gen/actor_model_generated.h
+src/mailbox.o: $(COMPONENT_PATH)/src/gen/actor_model_generated.h
 
-$(COMPONENT_PATH)/src/actor_model_generated.h: $(COMPONENT_PATH)/actor_model.fbs
+$(COMPONENT_PATH)/src/gen/actor_model_generated.h: $(COMPONENT_PATH)/actor_model.fbs
 	flatc --cpp -o $(@D) \
 	--scoped-enums \
 	--gen-mutable \
