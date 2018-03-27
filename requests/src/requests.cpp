40b94c4dbda732b3f7c74badebd508ebe7f0f719
Paul Reimer <paul@p-rimes.net>
2018-03-27 14:06:29 -0700

    ✨ Add requests/src/requests.h wrapper around common functionality, this is the public header that should be included by users.

---
+++ requests/src/requests.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright Paul Reimer, 2018
+ *
+ * All rights reserved.
+ *
+ */
+#include "requests.h"
+
+namespace Requests {
+
+using string_view = std::experimental::string_view;
+
+auto make_request(
+  string_view method,
+  string_view uri,
+  std::vector<std::pair<string_view, string_view>> query,
+  std::vector<std::pair<string_view, string_view>> headers,
+  string_view body
+)
+  -> RequestT
+{
+  RequestT req{};
+
+  update_request(
+    req,
+    method,
+    uri,
+    query,
+    headers,
+    body
+  );
+
+  return req;
+}
+
+auto update_request(
+  RequestT& req,
+  string_view method,
+  string_view uri,
+  std::vector<std::pair<string_view, string_view>> query,
+  std::vector<std::pair<string_view, string_view>> headers,
+  string_view body
+)
+  -> void
+{
+  req.method.assign(method.data(), method.size());
+  req.uri.assign(uri.data(), uri.size());
+
+  for (const auto& arg : query)
+  {
+    auto query_arg = std::make_unique<QueryPairT>();
+    query_arg->first.assign(arg.first.data(), arg.first.size());
+    query_arg->second.assign(arg.second.data(), arg.second.size());
+    req.query.emplace_back(std::move(query_arg));
+  }
+
+  for (const auto& hdr : headers)
+  {
+    auto header = std::make_unique<HeaderPairT>();
+    header->first.assign(hdr.first.data(), hdr.first.size());
+    header->second.assign(hdr.second.data(), hdr.second.size());
+    req.headers.emplace_back(std::move(header));
+  }
+
+  req.body.assign(body.data(), body.size());
+
+  // pending, ready, abort;
+  req.state = std::make_unique<RequestState>(false, true, false);
+}
+
+auto set_query_arg(
+  auto& query,
+  string_view first,
+  string_view second
+)
+  -> bool
+{
+  auto updated_existing_arg = false;
+
+  for (auto& arg : query)
+  {
+    if (arg->first == first)
+    {
+      arg->second.assign(second.data(), second.size());
+      updated_existing_arg = true;
+    }
+  }
+
+  if (not updated_existing_arg)
+  {
+    auto new_query_arg = std::make_unique<QueryPairT>();
+    new_query_arg->first.assign(first.data(), first.size());
+    new_query_arg->second.assign(second.data(), second.size());
+
+    query.emplace_back(std::move(new_query_arg));
+  }
+
+  return updated_existing_arg;
+}
+
+
+} // namespace ActorModel
---
+++ requests/src/requests.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright Paul Reimer, 2018
+ *
+ * All rights reserved.
+ *
+ */
+#pragma once
+
+#include "request_manager.h"
+//#include "requests_generated.h"
+
+#include <experimental/string_view>
+
+namespace Requests {
+
+auto make_request(
+  std::experimental::string_view method,
+  std::experimental::string_view uri,
+  std::vector<
+    std::pair<std::experimental::string_view, std::experimental::string_view>
+  > query = {},
+  std::vector<
+    std::pair<std::experimental::string_view, std::experimental::string_view>
+  > headers = {},
+  std::experimental::string_view body = ""
+)
+  -> RequestT;
+
+auto update_request(
+  RequestT& req,
+  std::experimental::string_view method,
+  std::experimental::string_view uri,
+  std::vector<
+    std::pair<std::experimental::string_view, std::experimental::string_view>
+  > query = {},
+  std::vector<
+    std::pair<std::experimental::string_view, std::experimental::string_view>
+  > headers = {},
+  std::experimental::string_view body = ""
+)
+  -> void;
+
+auto set_query_arg(
+  auto& query,
+  std::experimental::string_view first,
+  std::experimental::string_view second
+)
+  -> bool;
+
+} // namespace Requests
