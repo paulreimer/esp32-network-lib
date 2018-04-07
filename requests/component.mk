bd11b78caf8f0757a1ac194e6bffd90499beee52
Paul Reimer <paul@p-rimes.net>
2018-04-06 21:52:37 -0700

    🔧 Add 'CXXFLAGS += -D_GLIBCXX_USE_C99=1' when compiling requests/json_emitter.cpp

--- requests/component.mk
+++ requests/component.mk
@@ -22,6 +22,7 @@ COMPONENT_SRCDIRS := \
 	lib/yajl/src \
 	lib/zlib
 
+src/json_emitter.o: CXXFLAGS += -D_GLIBCXX_USE_C99=1
 src/request_handler.o: $(COMPONENT_PATH)/src/gen/requests_generated.h
 src/request_manager.o: $(COMPONENT_PATH)/src/gen/requests_generated.h
 src/request_manager.o: CXXFLAGS += -D_GLIBCXX_USE_C99=1
