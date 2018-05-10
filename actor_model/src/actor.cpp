546082456691b1ffb4d154a04f2154fb7b2e825b
Paul Reimer <paul@p-rimes.net>
2018-05-10 00:10:25 -0700

    📄 Use 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International' license consistently.

--- actor_model/src/actor.cpp
+++ actor_model/src/actor.cpp
@@ -1,8 +1,11 @@
 /*
  * Copyright Paul Reimer, 2018
  *
- * All rights reserved.
- *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License.
+ * To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  */
 #include "actor.h"
 
