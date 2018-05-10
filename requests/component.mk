1f87ef4dfb05a8e9d415f8752c43c202725dc8aa
Paul Reimer <paul@p-rimes.net>
2018-05-09 23:56:23 -0700

    💚 Add dependency on requests_generated.h to json_emitter.cpp in requests component.

--- requests/component.mk
+++ requests/component.mk
@@ -28,6 +28,7 @@ COMPONENT_EMBED_FILES := \
 
 src/requests.o: $(COMPONENT_PATH)/src/gen/requests.bfbs
 src/json_emitter.o: CXXFLAGS += -D_GLIBCXX_USE_C99=1
+src/json_emitter.o: $(COMPONENT_PATH)/src/gen/requests_generated.h
 src/request_handler.o: $(COMPONENT_PATH)/src/gen/requests_generated.h
 src/request_manager.o: $(COMPONENT_PATH)/src/gen/requests_generated.h
 src/request_manager.o: CXXFLAGS += -D_GLIBCXX_USE_C99=1
