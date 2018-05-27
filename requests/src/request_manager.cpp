4e6035a4065bc9e0999ab58b637d321f278756c4
Paul Reimer <paul@p-rimes.net>
2018-05-26 17:42:34 -0700

    🐛 Add null pointer check for RequestManager::send method to verify that at least a URI is present.

--- requests/src/request_manager.cpp
+++ requests/src/request_manager.cpp
@@ -224,264 +224,269 @@ auto RequestManager::send(
   RequestHandler& handler
 ) -> bool
 {
-  const auto& req = handler.request_intent->request();
+  const auto* req = handler.request_intent->request();
 
-  auto uri_str = req->uri()->string_view();
-  handler._req_url.assign(uri_str.begin(), uri_str.end());
-
-  if (req->query() and req->query()->size() > 0)
+  if (req and req->uri() and (req->uri()->size() > 0))
   {
-    char delim = (handler._req_url.find_first_of('?') == string::npos)? '?' : '&';
-    for (auto arg_idx = 0; arg_idx < req->query()->size(); ++arg_idx)
+    auto uri_str = req->uri()->string_view();
+    handler._req_url.assign(uri_str.begin(), uri_str.end());
+
+    if (req->query() and req->query()->size() > 0)
     {
-      const auto* arg = req->query()->Get(arg_idx);
-      handler._req_url += delim + urlencode(arg->k()->string_view()) + '=' + urlencode(arg->v()->string_view());
-      delim = '&';
+      char delim = (handler._req_url.find_first_of('?') == string::npos)? '?' : '&';
+      for (auto arg_idx = 0; arg_idx < req->query()->size(); ++arg_idx)
+      {
+        const auto* arg = req->query()->Get(arg_idx);
+        handler._req_url += delim + urlencode(arg->k()->string_view()) + '=' + urlencode(arg->v()->string_view());
+        delim = '&';
+      }
     }
-  }
 
 #ifdef REQUESTS_USE_CURL
-  // Allocate CURL_ERROR_SIZE bytes of zero-initialized data
-  handler.errbuf.resize(CURL_ERROR_SIZE, 0);
+    // Allocate CURL_ERROR_SIZE bytes of zero-initialized data
+    handler.errbuf.resize(CURL_ERROR_SIZE, 0);
 
-  // Reset the c-string contents to zero-length, null-terminated
-  handler.errbuf[0] = 0;
+    // Reset the c-string contents to zero-length, null-terminated
+    handler.errbuf[0] = 0;
 
-  auto* curl = handle;
-  curl_easy_setopt(curl, CURLOPT_URL, handler._req_url.c_str());
+    auto* curl = handle;
+    curl_easy_setopt(curl, CURLOPT_URL, handler._req_url.c_str());
 
-  // Do not print out any updates to stdout
-  curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
-  curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-  // Do not install signal handlers
-  curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
+    // Do not print out any updates to stdout
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+    // Do not install signal handlers
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
 
-  // Set user data pointer attached to this request
-  curl_easy_setopt(curl, CURLOPT_PRIVATE, &handler);
+    // Set user data pointer attached to this request
+    curl_easy_setopt(curl, CURLOPT_PRIVATE, &handler);
 
-  // Provide a buffer for curl to store error strings in
-  curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, handler.errbuf.data());
+    // Provide a buffer for curl to store error strings in
+    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, handler.errbuf.data());
 
-  // Follow redirects (3xx responses) until the actual URL is found
-  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    // Follow redirects (3xx responses) until the actual URL is found
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
-  // Attempt HTTP/2 for HTTPS URLs, fallback to HTTP/1.1 otherwise
+    // Attempt HTTP/2 for HTTPS URLs, fallback to HTTP/1.1 otherwise
 #if REQUESTS_SUPPORT_HTTP2
-  curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
 #else
-  curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 #endif
 
-  // Waiting for pending connections to be established, to multiplex if possible
-  curl_easy_setopt(curl, CURLOPT_PIPEWAIT, 1L);
+    // Waiting for pending connections to be established, to multiplex if possible
+    curl_easy_setopt(curl, CURLOPT_PIPEWAIT, 1L);
 
-  // Do not include headers in the response stream
-  curl_easy_setopt(curl, CURLOPT_HEADER, 0L);
-  // Parse headers with a separate callback
-  curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
-  // Set user data pointer attached to the header function for this request
-  curl_easy_setopt(curl, CURLOPT_HEADERDATA, &handler);
+    // Do not include headers in the response stream
+    curl_easy_setopt(curl, CURLOPT_HEADER, 0L);
+    // Parse headers with a separate callback
+    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header_callback);
+    // Set user data pointer attached to the header function for this request
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &handler);
 
-  // Body data incremental callback
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
-  // Set user data pointer attached to the write function for this request
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &handler);
+    // Body data incremental callback
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
+    // Set user data pointer attached to the write function for this request
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &handler);
 
-  // Verify SSL certificates with CA cert(s)
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
-  // Expect PEM formatted CA cert(s)
-  //curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE, "PEM");
-  // Use a function to supply PEM contents of CA cert(s) in memory buffer
-  curl_easy_setopt(curl, CURLOPT_SSL_CTX_FUNCTION, sslctx_function);
-  // Set user data pointer attached to the sslctx function for this request
-  curl_easy_setopt(curl, CURLOPT_SSL_CTX_DATA, this);
+    // Verify SSL certificates with CA cert(s)
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+    // Expect PEM formatted CA cert(s)
+    //curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE, "PEM");
+    // Use a function to supply PEM contents of CA cert(s) in memory buffer
+    curl_easy_setopt(curl, CURLOPT_SSL_CTX_FUNCTION, sslctx_function);
+    // Set user data pointer attached to the sslctx function for this request
+    curl_easy_setopt(curl, CURLOPT_SSL_CTX_DATA, this);
 
-  // Turn off the default CA locations, so no attempts are made to load them
-  curl_easy_setopt(curl, CURLOPT_CAINFO, NULL);
-  curl_easy_setopt(curl, CURLOPT_CAPATH, NULL);
+    // Turn off the default CA locations, so no attempts are made to load them
+    curl_easy_setopt(curl, CURLOPT_CAINFO, NULL);
+    curl_easy_setopt(curl, CURLOPT_CAPATH, NULL);
 
-  if (handler.slist)
-  {
-    // Reset existing headers
-    curl_slist_free_all(handler.slist);
-    handler.slist = nullptr;
-  }
-
-  if (req->headers() and req->headers()->size() > 0)
-  {
-    for (auto hdr_idx = 0; hdr_idx < req->headers()->size(); ++hdr_idx)
+    if (handler.slist)
     {
-      const auto* hdr = req->headers()->Get(hdr_idx);
-      string hdr_str(hdr->k()->str() + string(": ") + hdr->v()->str());
-      handler.slist = curl_slist_append(handler.slist, hdr_str.c_str());
+      // Reset existing headers
+      curl_slist_free_all(handler.slist);
+      handler.slist = nullptr;
     }
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, handler.slist);
-  }
 
-  if (req->method()->string_view() == "GET")
-  {
-    // Use GET (default)
-  }
-  else if (req->method()->string_view() == "POST")
-  {
-    // Use POST
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-  }
-  else {
-    // Use a custom (user-supplied) HTTP method (e.g. PATCH)
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, req->method()->c_str());
-  }
-
-  // Set the POST body data, if the request specifies a body
-  if (req->body() and req->body()->size() > 0)
-  {
-    // Specify the length of the post body
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, req->body()->size());
-    // Specify a pointer to the data of the post body, do not delete it
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req->body()->data());
-  }
-
-  const auto& tag = handler.request_intent->request()->uri()->c_str();
-  ESP_LOGI(tag, "%s", req->method()->c_str());
-  curl_multi_add_handle(multi_handle.get(), curl);
-
-  return true;
-#endif // REQUESTS_USE_CURL
-
-#ifdef REQUESTS_USE_SH2LIB
-  auto* hd = handle;
-
-  handler.body_sent_byte_count = 0;
-
-  // Parse the requested host name, to see if it has changed
-  struct http_parser_url u;
-  http_parser_url_init(&u);
-  auto url_data = handler._req_url.data();
-  http_parser_parse_url(url_data, handler._req_url.size(), 0, &u);
-
-  auto hostname = string_view(
-    &url_data[u.field_data[UF_HOST].off],
-    u.field_data[UF_HOST].len
-  );
-
-  auto full_path_len = u.field_data[UF_PATH].len;
-
-  // Check for query, include the '?'
-  if (u.field_data[UF_QUERY].len)
-  {
-    full_path_len += 1 + u.field_data[UF_QUERY].len;
-  }
-  // Check for fragment, include the '#'
-  if (u.field_data[UF_FRAGMENT].len)
-  {
-    full_path_len += 1 + u.field_data[UF_FRAGMENT].len;
-  }
-
-  handler._path = string_view{
-    &url_data[u.field_data[UF_PATH].off],
-    full_path_len
-  };
-
-  // Initialize the HTTP/2 connection
-  if (
-    handler.connected_hostname != hostname
-    or not handler.connected
-  )
-  {
-    // Reset the current hostname
-    handler.connected_hostname.clear();
-
-    handler.connected = (
-      sh2lib_connect(
-        hd,
-        req->uri()->c_str(),
-        &cacerts
-      ) == 0
-    );
-
-    if (handler.connected)
+    if (req->headers() and req->headers()->size() > 0)
     {
-      // Create pointer so handle callback can access handler methods
-      handle->userdata = &handler;
-
-      // Set the cached connected hostname
-      handler.connected_hostname.assign(
-        hostname.begin(),
-        hostname.end()
-      );
-    }
-  }
-
-  // Only proceed if we are connected
-  if (handler.connected)
-  {
-    uint8_t flags = {
-      NGHTTP2_NV_FLAG_NO_COPY_NAME
-      | NGHTTP2_NV_FLAG_NO_COPY_VALUE
-    };
-
-    std::vector<nghttp2_nv> nva_vec = {
-      SH2LIB_MAKE_NV(":method", req->method()->c_str(), flags),
+      for (auto hdr_idx = 0; hdr_idx < req->headers()->size(); ++hdr_idx)
       {
-        (uint8_t *)":path",
-        (uint8_t *)handler._path.data(),
-        strlen(":path"),
-        handler._path.size(),
-        flags
-      },
-      SH2LIB_MAKE_NV(":scheme", "https", flags),
-      SH2LIB_MAKE_NV(":authority", handler.connected_hostname.c_str(), flags),
-    };
-
-    for (auto hdr_idx = 0; hdr_idx < req->headers()->size(); ++hdr_idx)
-    {
-      const auto* hdr = req->headers()->Get(hdr_idx);
-      nva_vec.emplace_back(nghttp2_nv{
-        (uint8_t*)(hdr->k()->data()),
-        (uint8_t*)(hdr->v()->data()),
-        hdr->k()->size(),
-        hdr->v()->size(),
-        flags
-      });
+        const auto* hdr = req->headers()->Get(hdr_idx);
+        string hdr_str(hdr->k()->str() + string(": ") + hdr->v()->str());
+        handler.slist = curl_slist_append(handler.slist, hdr_str.c_str());
+      }
+      curl_easy_setopt(curl, CURLOPT_HTTPHEADER, handler.slist);
     }
 
-    // Make the request
     if (req->method()->string_view() == "GET")
     {
       // Use GET (default)
-      auto retval = sh2lib_do_get_with_nv(
-        hd,
-        nva_vec.data(),
-        nva_vec.size(),
-        &callbacks
-      );
+    }
+    else if (req->method()->string_view() == "POST")
+    {
+      // Use POST
+      curl_easy_setopt(curl, CURLOPT_POST, 1L);
     }
     else {
-      // Add content length header with request body size
-      auto content_length = std::to_string(req->body()->size());
-
-      nva_vec.emplace_back(nghttp2_nv{
-        (uint8_t*)"Content-Length",
-        (uint8_t*)content_length.data(),
-        strlen("Content-Length"),
-        content_length.size(),
-        flags
-      });
-
       // Use a custom (user-supplied) HTTP method (e.g. PATCH)
-      auto retval = sh2lib_do_putpost_with_nv(
-        hd,
-        nva_vec.data(),
-        nva_vec.size(),
-        &callbacks
-      );
+      curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, req->method()->c_str());
     }
+
+    // Set the POST body data, if the request specifies a body
+    if (req->body() and req->body()->size() > 0)
+    {
+      // Specify the length of the post body
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, req->body()->size());
+      // Specify a pointer to the data of the post body, do not delete it
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req->body()->data());
+    }
+
+    const auto& tag = handler.request_intent->request()->uri()->c_str();
+    ESP_LOGI(tag, "%s", req->method()->c_str());
+    curl_multi_add_handle(multi_handle.get(), curl);
+
+    return true;
+#endif // REQUESTS_USE_CURL
+
+#ifdef REQUESTS_USE_SH2LIB
+    auto* hd = handle;
+
+    handler.body_sent_byte_count = 0;
+
+    // Parse the requested host name, to see if it has changed
+    struct http_parser_url u;
+    http_parser_url_init(&u);
+    auto url_data = handler._req_url.data();
+    http_parser_parse_url(url_data, handler._req_url.size(), 0, &u);
+
+    auto hostname = string_view(
+      &url_data[u.field_data[UF_HOST].off],
+      u.field_data[UF_HOST].len
+    );
+
+    auto full_path_len = u.field_data[UF_PATH].len;
+
+    // Check for query, include the '?'
+    if (u.field_data[UF_QUERY].len)
+    {
+      full_path_len += 1 + u.field_data[UF_QUERY].len;
+    }
+    // Check for fragment, include the '#'
+    if (u.field_data[UF_FRAGMENT].len)
+    {
+      full_path_len += 1 + u.field_data[UF_FRAGMENT].len;
+    }
+
+    handler._path = string_view{
+      &url_data[u.field_data[UF_PATH].off],
+      full_path_len
+    };
+
+    // Initialize the HTTP/2 connection
+    if (
+      handler.connected_hostname != hostname
+      or not handler.connected
+    )
+    {
+      // Reset the current hostname
+      handler.connected_hostname.clear();
+
+      handler.connected = (
+        sh2lib_connect(
+          hd,
+          req->uri()->c_str(),
+          &cacerts
+        ) == 0
+      );
+
+      if (handler.connected)
+      {
+        // Create pointer so handle callback can access handler methods
+        handle->userdata = &handler;
+
+        // Set the cached connected hostname
+        handler.connected_hostname.assign(
+          hostname.begin(),
+          hostname.end()
+        );
+      }
+    }
+
+    // Only proceed if we are connected
+    if (handler.connected)
+    {
+      uint8_t flags = {
+        NGHTTP2_NV_FLAG_NO_COPY_NAME
+        | NGHTTP2_NV_FLAG_NO_COPY_VALUE
+      };
+
+      std::vector<nghttp2_nv> nva_vec = {
+        SH2LIB_MAKE_NV(":method", req->method()->c_str(), flags),
+        {
+          (uint8_t *)":path",
+          (uint8_t *)handler._path.data(),
+          strlen(":path"),
+          handler._path.size(),
+          flags
+        },
+        SH2LIB_MAKE_NV(":scheme", "https", flags),
+        SH2LIB_MAKE_NV(":authority", handler.connected_hostname.c_str(), flags),
+      };
+
+      for (auto hdr_idx = 0; hdr_idx < req->headers()->size(); ++hdr_idx)
+      {
+        const auto* hdr = req->headers()->Get(hdr_idx);
+        nva_vec.emplace_back(nghttp2_nv{
+          (uint8_t*)(hdr->k()->data()),
+          (uint8_t*)(hdr->v()->data()),
+          hdr->k()->size(),
+          hdr->v()->size(),
+          flags
+        });
+      }
+
+      // Make the request
+      if (req->method()->string_view() == "GET")
+      {
+        // Use GET (default)
+        auto retval = sh2lib_do_get_with_nv(
+          hd,
+          nva_vec.data(),
+          nva_vec.size(),
+          &callbacks
+        );
+      }
+      else {
+        // Add content length header with request body size
+        auto content_length = std::to_string(req->body()->size());
+
+        nva_vec.emplace_back(nghttp2_nv{
+          (uint8_t*)"Content-Length",
+          (uint8_t*)content_length.data(),
+          strlen("Content-Length"),
+          content_length.size(),
+          flags
+        });
+
+        // Use a custom (user-supplied) HTTP method (e.g. PATCH)
+        auto retval = sh2lib_do_putpost_with_nv(
+          hd,
+          nva_vec.data(),
+          nva_vec.size(),
+          &callbacks
+        );
+      }
+    }
+    else {
+      ESP_LOGE(TAG, "%s %s: not connected", req->method()->c_str(), req->uri()->c_str());
+    }
+#endif // REQUESTS_USE_SH2LIB
   }
   else {
-    ESP_LOGE(TAG, "%s %s: not connected", req->method()->c_str(), req->uri()->c_str());
+    ESP_LOGE(TAG, "send: Invalid or missing request URI");
   }
-
-#endif // REQUESTS_USE_SH2LIB
 
   return false;
 }
