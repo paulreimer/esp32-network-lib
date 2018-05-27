18469a52f47a515f1dcb4ad124832458bf58afae
Paul Reimer <paul@p-rimes.net>
2018-05-26 18:06:24 -0700

    📄 Add license text to top of files in utils component

--- utils/src/timestamp.cpp
+++ utils/src/timestamp.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright Paul Reimer, 2018
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 Unported License.
+ * To view a copy of this license, visit
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or send a letter to
+ * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+ */
 #include "timestamp.h"
 
 #include "esp_timer.h"
