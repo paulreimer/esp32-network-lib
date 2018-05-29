db0e3cfd288527774ae1c7db096be7a483928a73
Paul Reimer <paul@p-rimes.net>
2018-05-29 13:28:28 -0700

    🎨 Add id and to_pid to InsertRowIntent in googleapis component

--- googleapis/src/gen/sheets_generated.h
+++ googleapis/src/gen/sheets_generated.h
@@ -6,6 +6,8 @@
 
 #include "flatbuffers/flatbuffers.h"
 
+#include "uuid_generated.h"
+
 namespace googleapis {
 namespace Sheets {
 
@@ -21,10 +23,24 @@ struct InsertRowIntent FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
     return "googleapis.Sheets.InsertRowIntent";
   }
   enum {
-    VT_SPREADSHEET_ID = 4,
-    VT_SHEET_NAME = 6,
-    VT_VALUES_JSON = 8
+    VT_ID = 4,
+    VT_TO_PID = 6,
+    VT_SPREADSHEET_ID = 8,
+    VT_SHEET_NAME = 10,
+    VT_VALUES_JSON = 12
   };
+  const UUID::UUID *id() const {
+    return GetStruct<const UUID::UUID *>(VT_ID);
+  }
+  UUID::UUID *mutable_id() {
+    return GetStruct<UUID::UUID *>(VT_ID);
+  }
+  const UUID::UUID *to_pid() const {
+    return GetStruct<const UUID::UUID *>(VT_TO_PID);
+  }
+  UUID::UUID *mutable_to_pid() {
+    return GetStruct<UUID::UUID *>(VT_TO_PID);
+  }
   const flatbuffers::String *spreadsheet_id() const {
     return GetPointer<const flatbuffers::String *>(VT_SPREADSHEET_ID);
   }
@@ -45,6 +61,8 @@ struct InsertRowIntent FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
   }
   bool Verify(flatbuffers::Verifier &verifier) const {
     return VerifyTableStart(verifier) &&
+           VerifyField<UUID::UUID>(verifier, VT_ID) &&
+           VerifyField<UUID::UUID>(verifier, VT_TO_PID) &&
            VerifyOffset(verifier, VT_SPREADSHEET_ID) &&
            verifier.Verify(spreadsheet_id()) &&
            VerifyOffset(verifier, VT_SHEET_NAME) &&
@@ -58,6 +76,12 @@ struct InsertRowIntent FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
 struct InsertRowIntentBuilder {
   flatbuffers::FlatBufferBuilder &fbb_;
   flatbuffers::uoffset_t start_;
+  void add_id(const UUID::UUID *id) {
+    fbb_.AddStruct(InsertRowIntent::VT_ID, id);
+  }
+  void add_to_pid(const UUID::UUID *to_pid) {
+    fbb_.AddStruct(InsertRowIntent::VT_TO_PID, to_pid);
+  }
   void add_spreadsheet_id(flatbuffers::Offset<flatbuffers::String> spreadsheet_id) {
     fbb_.AddOffset(InsertRowIntent::VT_SPREADSHEET_ID, spreadsheet_id);
   }
@@ -81,6 +105,8 @@ struct InsertRowIntentBuilder {
 
 inline flatbuffers::Offset<InsertRowIntent> CreateInsertRowIntent(
     flatbuffers::FlatBufferBuilder &_fbb,
+    const UUID::UUID *id = 0,
+    const UUID::UUID *to_pid = 0,
     flatbuffers::Offset<flatbuffers::String> spreadsheet_id = 0,
     flatbuffers::Offset<flatbuffers::String> sheet_name = 0,
     flatbuffers::Offset<flatbuffers::String> values_json = 0) {
@@ -88,16 +114,22 @@ inline flatbuffers::Offset<InsertRowIntent> CreateInsertRowIntent(
   builder_.add_values_json(values_json);
   builder_.add_sheet_name(sheet_name);
   builder_.add_spreadsheet_id(spreadsheet_id);
+  builder_.add_to_pid(to_pid);
+  builder_.add_id(id);
   return builder_.Finish();
 }
 
 inline flatbuffers::Offset<InsertRowIntent> CreateInsertRowIntentDirect(
     flatbuffers::FlatBufferBuilder &_fbb,
+    const UUID::UUID *id = 0,
+    const UUID::UUID *to_pid = 0,
     const char *spreadsheet_id = nullptr,
     const char *sheet_name = nullptr,
     const char *values_json = nullptr) {
   return googleapis::Sheets::CreateInsertRowIntent(
       _fbb,
+      id,
+      to_pid,
       spreadsheet_id ? _fbb.CreateString(spreadsheet_id) : 0,
       sheet_name ? _fbb.CreateString(sheet_name) : 0,
       values_json ? _fbb.CreateString(values_json) : 0);
@@ -105,17 +137,24 @@ inline flatbuffers::Offset<InsertRowIntent> CreateInsertRowIntentDirect(
 
 inline const flatbuffers::TypeTable *InsertRowIntentTypeTable() {
   static const flatbuffers::TypeCode type_codes[] = {
+    { flatbuffers::ET_SEQUENCE, 0, 0 },
+    { flatbuffers::ET_SEQUENCE, 0, 0 },
     { flatbuffers::ET_STRING, 0, -1 },
     { flatbuffers::ET_STRING, 0, -1 },
     { flatbuffers::ET_STRING, 0, -1 }
   };
+  static const flatbuffers::TypeFunction type_refs[] = {
+    UUID::UUIDTypeTable
+  };
   static const char * const names[] = {
+    "id",
+    "to_pid",
     "spreadsheet_id",
     "sheet_name",
     "values_json"
   };
   static const flatbuffers::TypeTable tt = {
-    flatbuffers::ST_TABLE, 3, type_codes, nullptr, nullptr, names
+    flatbuffers::ST_TABLE, 5, type_codes, type_refs, nullptr, names
   };
   return &tt;
 }
